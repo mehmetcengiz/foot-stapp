a13ad9b834090df18ed714548bae917cf8f88532
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-05-07 19:32:16 +0300

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,45 @@
+angular.module('footstapp.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('HomeCtrl', function($scope, $stateParams, HomeCards) {
+  $scope.homecards = HomeCards.all();
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+
+
+})
+
+.controller('LoginCtrl', function($scope) {
+    $scope.data = {};
+ 
+    $scope.login = function() {
+        console.log("LOGIN user: " + $scope.data.username + " - PW: " + $scope.data.password);
+    }
+    $scope.signup = function(){
+        console.log("ASDASDASDASDASD");
+    }
+});
---
+++ www/js/services.js
@@ -0,0 +1,124 @@
+angular.module("footstapp.services", [])
+
+.service("LoginService", function($q) {
+  return {
+    loginUser: function(name, pw) {
+      var deferred = $q.defer();
+      var promise = deferred.promise;
+ 
+      if (name == "user" && pw == "secret") {
+        deferred.resolve("Welcome " + name + "!");
+      } else {
+        deferred.reject("Wrong credentials.");
+      }
+      promise.success = function(fn) {
+        promise.then(fn);
+        return promise;
+      }
+      promise.error = function(fn) {
+        promise.then(null, fn);
+        return promise;
+      }
+      return promise;
+    }
+  }
+})
+
+.factory("HomeCards", function() {
+  var homecards = [
+    {
+      id: 0,
+      avatar: "img/max.png",
+      name: "Ozan Müyesseroğlu",
+      eventName: "test event",
+      eventDescription: "test Description",
+      eventPhoto: "img/adam.jpg",
+      eventMessage: "Test event Message"
+    },
+    {
+      id: 1,
+      avatar: "img/mike.png",
+      name: "Mehmet Cengiz",
+      eventName: "test event",
+      eventDescription: "test Description",
+      eventPhoto: "img/ben.png",
+      eventMessage: "Test event Message"
+    },
+    {
+      id: 2,
+      avatar: "img/perry.png",
+      name: "Emre Danışan",
+      eventName: "test event",
+      eventDescription: "test Description",
+      eventPhoto: "img/ionic.png",
+      eventMessage: "Test event Message"
+    }
+  ];
+
+  return {
+  all: function() {
+    return homecards;
+  },
+  remove: function(card) {
+    homecards.splice(homecards.indexOf(card), 1);
+  },
+  get: function(cardId) {
+console.log(cardId);      
+    for (var i = 0; i < homecards.length; i++) {
+    if (homecards[i].id === parseInt(cardId)) {
+      return homecards[i];
+    }
+    }
+    return null;
+  }
+  };
+})
+
+.factory("Chats", function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+  id: 0,
+  name: "Ben Sparrow",
+  lastText: "You on your way?",
+  face: "img/ben.png"
+  }, {
+  id: 1,
+  name: "Max Lynx",
+  lastText: "Hey, it\"s me",
+  face: "img/max.png"
+  }, {
+  id: 2,
+  name: "Adam Bradleyson",
+  lastText: "I should buy a boat",
+  face: "img/adam.jpg"
+  }, {
+  id: 3,
+  name: "Perry Governor",
+  lastText: "Look at my mukluks!",
+  face: "img/perry.png"
+  }, {
+  id: 4,
+  name: "Mike Harrington",
+  lastText: "This is wicked good ice cream.",
+  face: "img/mike.png"
+  }];
+
+  return {
+  all: function() {
+    return chats;
+  },
+  remove: function(chat) {
+    chats.splice(chats.indexOf(chat), 1);
+  },
+  get: function(chatId) {
+    for (var i = 0; i < chats.length; i++) {
+    if (chats[i].id === parseInt(chatId)) {
+      return chats[i];
+    }
+    }
+    return null;
+  }
+  };
+});
